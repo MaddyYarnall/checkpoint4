81a7dd35fa36beef6d196641b35e1da7f92b1193
Madison Yarnall <maddycodes01@gmail.com>
2022-01-07 21:02:46 -0700

    quotes still not working

--- checkpoint4/app/Models/Quotes.js
+++ checkpoint4/app/Models/Quotes.js
@@ -1,8 +1,8 @@
 export class Quotes {
   constructor(data) {
-    this.id = data.id,
-      this.content = data.content,
-      this.author = data.author
+    this.id = data.id
+    this.content = data.content
+    this.author = data.author
   }
 
   get Template() {
